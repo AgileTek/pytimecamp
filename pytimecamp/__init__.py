735ad9f00f9b012a58053f80fe72cbdb08e7f9d7
Steven Rossiter <steve@agiletek.co.uk>
2016-11-25 11:43:23 +0000

    v 0.1.6

--- pytimecamp/__init__.py
+++ pytimecamp/__init__.py
@@ -1,5 +1,5 @@
 __author__ = 'agiletekengineering'
-__version__ = '0.1.1'
+__version__ = '0.1.4'
 import datetime as dt
 
 import dateutil.relativedelta as rdelta
--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 import os
 from setuptools import setup
-README = os.path.join(os.path.dirname(__file__), 'README.md')
-try:
-    import pypandoc
-    LONG = pypandoc.convert(README,format='md', to='rst')
-except ImportError as e:
-    print (e)
-    LONG = open(README).read()
 
 setup(
     name="pytimecamp",
-    version="0.1.4",
+    version="0.1.6",
     author="Steven Rossiter",
     author_email="steve@agiletek.co.uk",
     description=("A wrapper around the Timecamp API."),
     license = "MIT",
     url = "https://pythonhosted.org/pytimecamp/",
     packages=['pytimecamp'],
-    long_description=LONG,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Win32 (MS Windows)",
